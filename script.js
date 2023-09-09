03c19e40f2c18c814415717a31dd27455b09fd59
Rose <rosebeats09@gmail.com>
2023-09-08 20:14:14 -0400

    Create object constructor, array and loop for the books

---
+++ script.js
@@ -0,0 +1,23 @@
+
+//constructor for making book objects
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+let book1 = new Book('The Children of Húrin', 'J.R.R. Tolkien', 320, 'true' )
+let book2 = new Book('Beren and Lúthien', 'J.R.R. Tolkien', 297, 'true' )
+let book3 = new Book('The Silmarillion', 'J.R.R. Tolkien', 365, 'true' )
+//library array
+const myLibrary = [book1, book2, book3];
+
+//function that adds the book/s to the library
+function addBookToLibrary() {
+    myLibrary.push(userInput);
+}
+
+//loop through array
+for (let i = 0; i < myLibrary.length; i++) {
+    console.log(myLibrary[i])
+}
