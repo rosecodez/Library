6bd349150eaca87240dd5ca0d31069038bc6c797
Rose <rosebeats09@gmail.com>
2023-09-09 17:07:56 -0400

    Rewrite table with DOM creation

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //access elements with DOM
 const button = document.querySelector("button");
-const submit = document.getElementById("submit")
-const table = document.getElementById("table-container");
+const submit = document.getElementById("submit");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
 
 //constructor for making book objects
 function Book(title, author, pages, read) {
@@ -10,9 +12,9 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
-let book1 = new Book('The Children of Húrin', 'J.R.R. Tolkien', 320, true )
-let book2 = new Book('Beren and Lúthien', 'J.R.R. Tolkien', 297, true )
-let book3 = new Book('The Silmarillion', 'J.R.R. Tolkien', 365, true )
+let book1 = new Book('The Children of Húrin', 'J.R.R. Tolkien', 320, true );
+let book2 = new Book('Beren and Lúthien', 'J.R.R. Tolkien', 297, true );
+let book3 = new Book('The Silmarillion', 'J.R.R. Tolkien', 365, true );
 
 //library array
 const myLibrary = [book1, book2, book3];
@@ -33,8 +35,20 @@ button.addEventListener("click", () => {
 
 submit.addEventListener("click", function (event) {
     event.preventDefault();
-    displayTable()
 });
-function displayTable() {
-    table.style.display = "table";
+
+const tableContainer = document.getElementById("table-container");
+//create table row with dom
+function createTable() {
+    //table
+    const table = document.createElement("table");
+    tableContainer.appendChild(table);
+    
+    //row
+    const row = document.createElement("tr");
+    table.appendChild(row);
+    //th
+    const th = document.createElement("th");
+    row.appendChild(th);
 }
+createTable();
