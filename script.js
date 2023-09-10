c433d3c8e7697d6a5699d542f3460cbb62533d22
Rose <rosebeats09@gmail.com>
2023-09-10 16:09:02 -0400

    Create function to create the delete button for each row

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //access elements with DOM
+const table = document.getElementById("table");
+const tableContainer = document.getElementById("table-container");
 const button = document.querySelector("button");
 const confirm = document.getElementById("confirm");
 const title = document.getElementById("title");
@@ -16,29 +18,26 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
-let book1 = new Book('The Children of Húrin', 'J.R.R. Tolkien', 320, true );
-let book2 = new Book('Beren and Lúthien', 'J.R.R. Tolkien', 297, true );
-let book3 = new Book('The Silmarillion', 'J.R.R. Tolkien', 365, true );
 
 //library array
-const myLibrary = [book1, book2, book3];
-
+const myLibrary = [];
+let userInput;
 //function that adds the book/s to the library
 function addBookToLibrary() {
-    myLibrary.push(userInput);
+    myLibrary.push(book1);
 }
 
 //loop through array
 for (let i = 0; i < myLibrary.length; i++) {
     console.log(myLibrary[i])
 }
-
 button.addEventListener("click", () => {
     dialog.showModal();
     displayTable();
 });
 
 confirm.addEventListener("click", function (event) {
+    createRow();
     event.preventDefault();
     dialog.close();
     th1.textContent = title.value;
@@ -51,25 +50,22 @@ confirm.addEventListener("click", function (event) {
     }
 });
 
+
 function displayTable() {
     tableContainer.style.display = "flex";
 }
-const tableContainer = document.getElementById("table-container");
+
 //create table row with dom
-function createTable() {
-    //table
-    let table = document.createElement("table");
-    tableContainer.appendChild(table);
-    table.setAttribute("id", "table");
-    //row
+function createRow() {
     let row = document.createElement("tr");
     table.appendChild(row);
     row.setAttribute("id", "row");
-    //th
+
+    //table headers
     let th1 = document.createElement("th");
     row.appendChild(th1);
     th1.setAttribute("id", "th1");
-
+    
     let th2 = document.createElement("th");
     row.appendChild(th2);
     th2.setAttribute("id", "th2");
@@ -81,5 +77,13 @@ function createTable() {
     let th4 = document.createElement("th");
     row.appendChild(th4);
     th4.setAttribute("id", "th4");
+
+    let deleteBtn = document.createElement("button");
+    row.appendChild(deleteBtn);
+    deleteBtn.setAttribute("id", "delete")
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener("click", function (event) {
+        row.remove(); 
+    })
 }
-createTable();
