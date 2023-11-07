cfc6c8b1f133aab0423ad8d9f0205efb9f588bf5
Rose <rosebeats09@gmail.com>
2023-11-06 23:24:32 -0500

    Add form validation

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 let count = 0;
 
-//access elements with DOM
-const table = document.getElementById("table");
-const tableContainer = document.getElementById("table-container");
-const button = document.querySelector("button");
-const confirm = document.getElementById("confirm");
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const pages = document.getElementById("pages");
-const dialog = document.querySelector("dialog");
-const readed = document.getElementById("readed");
-const notreaded = document.getElementById("notreaded");
-const cancel = document.getElementById("cancel");
+// access elements with DOM
+const table = document.getElementById('table');
+const tableContainer = document.getElementById('table-container');
+const button = document.querySelector('button');
+const confirm = document.getElementById('confirm');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const dialog = document.querySelector('dialog');
+const readed = document.getElementById('readed');
+const notreaded = document.getElementById('notreaded');
 
-//constructor for making book objects
+// constructor for making book objects
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -21,86 +20,83 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-//library array
+// library array
 const myLibrary = [];
 let userInput;
-//function that adds the book/s to the library
+// function that adds the book/s to the library
 function addBookToLibrary() {
-    myLibrary.push(new Book);
+  myLibrary.push(new Book());
 }
 
-//loop through array
+// loop through array
 for (let i = 0; i < myLibrary.length; i++) {
-    console.log(myLibrary[i])
+  console.log(myLibrary[i]);
 }
-button.addEventListener("click", () => {
-    dialog.showModal();
-    displayTable();
-});
-
-confirm.addEventListener("click", function (event) {
-    createRow(count);
-    const th1 = document.querySelector("#th1-"+count);
-    const th2 = document.querySelector("#th2-"+count);
-    const th3 = document.querySelector("#th3-"+count);
-    const th4 = document.querySelector("#th4-"+count);
-    event.preventDefault();
-    dialog.close();
-    th1.textContent = title.value;
-    th2.textContent = author.value;
-    th3.textContent = pages.value;
-    if(readed.checked) {
-        th4.textContent = readed.value;
-    } else if (notreaded.checked) {
-        th4.textContent = notreaded.value;
-    }
-    
-});
-confirm.onclick = function () {
-    count++;
-    console.log(count);
-    if (count <=2) {
-        title.value = "";
-        author.value = "";
-        pages.value = "";
-        readed.checked = false;
-        notreaded.checked = false;
-    }
-}
-
 function displayTable() {
-    tableContainer.style.display = "flex";
+  tableContainer.style.display = 'flex';
 }
-
-//create table row with dom
+button.addEventListener('click', () => {
+  dialog.showModal();
+  displayTable();
+});
+// create table row with dom
 function createRow(num) {
-    let row = document.createElement("tr");
-    table.appendChild(row);
-    row.setAttribute("id", "row"+num);
+  const row = document.createElement('tr');
+  table.appendChild(row);
+  row.setAttribute('id', `row${num}`);
 
-    //table headers
-    let th1 = document.createElement("th");
-    row.appendChild(th1);
-    th1.setAttribute("id", "th1-"+num);
-    
-    let th2 = document.createElement("th");
-    row.appendChild(th2);
-    th2.setAttribute("id", "th2-"+num);
+  // table headers
+  const th1 = document.createElement('th');
+  row.appendChild(th1);
+  th1.setAttribute('id', `th1-${num}`);
 
-    let th3 = document.createElement("th");
-    row.appendChild(th3);
-    th3.setAttribute("id", "th3-"+num);
+  const th2 = document.createElement('th');
+  row.appendChild(th2);
+  th2.setAttribute('id', `th2-${num}`);
 
-    let th4 = document.createElement("th");
-    row.appendChild(th4);
-    th4.setAttribute("id", "th4-"+num);
+  const th3 = document.createElement('th');
+  row.appendChild(th3);
+  th3.setAttribute('id', `th3-${num}`);
 
-    let deleteBtn = document.createElement("button");
-    row.appendChild(deleteBtn);
-    deleteBtn.setAttribute("class", "delete")
-    deleteBtn.textContent = "Delete";
+  const th4 = document.createElement('th');
+  row.appendChild(th4);
+  th4.setAttribute('id', `th4-${num}`);
 
-    deleteBtn.addEventListener("click", function (event) {
-        row.remove(); 
-    })
+  const deleteBtn = document.createElement('button');
+  row.appendChild(deleteBtn);
+  deleteBtn.setAttribute('class', 'delete');
+  deleteBtn.textContent = 'Delete';
+
+  deleteBtn.addEventListener('click', () => {
+    row.remove();
+  });
 }
+confirm.addEventListener('click', (event) => {
+  createRow(count);
+  const th1 = document.querySelector(`#th1-${count}`);
+  const th2 = document.querySelector(`#th2-${count}`);
+  const th3 = document.querySelector(`#th3-${count}`);
+  const th4 = document.querySelector(`#th4-${count}`);
+  event.preventDefault();
+  dialog.close();
+  th1.textContent = title.value;
+  th2.textContent = author.value;
+  th3.textContent = pages.value;
+  if (readed.checked) {
+    th4.textContent = readed.value;
+  } else if (notreaded.checked) {
+    th4.textContent = notreaded.value;
+  }
+});
+
+confirm.onclick = function () {
+  count++;
+  console.log(count);
+  if (count <= 2) {
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    readed.checked = false;
+    notreaded.checked = false;
+  }
+};
